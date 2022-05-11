af1c422da5ffd22101a104933281847780331c55
Andrew Antaya <aantaya@email.arizona.edu>
2022-05-11 05:02:09 -0700

    learning about inputs and outputs

--- app/app.R
+++ app/app.R
@@ -1,25 +1,16 @@
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput("x", "If x is", min = 1, max = 50, value = 30),
-  sliderInput("y", "and y is", min = 1, max = 50, value = 5),
-  "then, (x * y) is", textOutput("product"),
-  "and, (x * y) + 5 is", textOutput("product_plus5"),
-  "and (x * y) + 10 is", textOutput("product_plus10")
+  textInput("name", "What's your name?"),
+  passwordInput("password", "What's your password?"),
+  textAreaInput("story", "Tell me about yourself", rows = 3),
+  textOutput("name")
 )
 
 server <- function(input, output, session) {
-  output$product <- renderText({
-    product <- input$x * input$y
-    product
-  })
-  output$product_plus5 <- renderText({
-    product <- input$x * input$y
-    product + 5
-  })
-  output$product_plus10 <- renderText({
-    product <- input$x * input$y
-    product + 10
+  output$name <- renderPrint({
+    name <- input$name
+    paste0("My name is ", name)
   })
 }
 
