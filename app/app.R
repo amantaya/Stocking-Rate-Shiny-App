e4c188b28e881dea5c95c3f2ccc5f50509ab0f2b
Andrew Antaya <aantaya@email.arizona.edu>
2022-05-31 16:46:09 -0700

    learning about password inputs and outputs

--- app/app.R
+++ app/app.R
@@ -1,16 +1,26 @@
 library(shiny)
 
 ui <- fluidPage(
-  textInput("name", "What's your name?"),
+  textInput("name", "What's your name?", placeholder = "My Name Is..."),
   passwordInput("password", "What's your password?"),
   textAreaInput("story", "Tell me about yourself", rows = 3),
-  textOutput("name")
+  textOutput("name"),
+  textOutput("password"),
+  textOutput("story")
 )
 
 server <- function(input, output, session) {
   output$name <- renderPrint({
     name <- input$name
     paste0("My name is ", name)
+  })
+  output$password <- renderPrint({
+    password <- input$password
+    password
+  })
+  output$story <- renderPrint({
+    story <- input$story
+    story
   })
 }
 
